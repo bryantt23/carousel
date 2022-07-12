67cfb2ffd7d29d7ad7d6d2ce2d43ce290d7570e3
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-07-11 19:13:55 -0700

    add comments

--- src/App.js
+++ src/App.js
@@ -3,21 +3,58 @@ import './App.css';
 import { getDogs } from './dogapi';
 import React, { useEffect, useState } from 'react';
 
-const size = 3;
+const size = 10;
 // TODO replace with your slideshow App
 const App = () => {
   const [dogs, setDogs] = useState([]);
   const [index, setIndex] = useState(0);
+  const [comment, setComment] = useState('');
+
+  const getDogsWithCommentsAndUpvotes = arr => {
+    return arr.map(elem => {
+      return {
+        ...elem,
+        comments: [
+          // { comment: "placeholder one", upvoteCount: 1 },
+          // { comment: "placeholder two", upvoteCount: 200 },
+          // { comment: "placeholder three", upvoteCount: 3 }
+        ]
+      };
+    });
+  };
 
   useEffect(() => {
     async function fetch() {
       const dogsTemp = await getDogs(size);
-      setDogs(dogsTemp);
+      console.log('dogsTemp', dogsTemp);
+      const dogsWithCommentsAndUpvotes =
+        getDogsWithCommentsAndUpvotes(dogsTemp);
+      console.log('dogsWithCommentsAndUpvotes', dogsWithCommentsAndUpvotes);
+      setDogs(dogsWithCommentsAndUpvotes);
     }
     console.log('hi');
 
     fetch();
   }, []);
+
+  const addComment = () => {
+    const curDog = dogs[index];
+    // debugger;
+    curDog.comments.push({ comment: comment, upvoteCount: 0 });
+    const dogsCopy = JSON.parse(JSON.stringify(dogs));
+    dogsCopy[index] = curDog;
+    setDogs(dogsCopy);
+    setComment('');
+  };
+
+  const increaseVote = commentIndex => {
+    console.log('commentIndex ', commentIndex);
+    const curDog = dogs[index];
+    curDog.comments[commentIndex].upvoteCount++;
+    const dogsCopy = JSON.parse(JSON.stringify(dogs));
+    dogsCopy[index] = curDog;
+    setDogs(dogsCopy);
+  };
 
   const changeIndex = index => {
     let indexTemp;
@@ -33,67 +70,126 @@ const App = () => {
   return (
     <section>
       {dogs.length > 0 ? (
-        <div
-          style={{
-            display: 'grid',
-            gridTemplateColumns: '1fr 4fr 1fr',
-            height: 300
-          }}
-        >
+        <div>
           <div
             style={{
-              display: 'flex',
-              justifyContent: 'flex-end'
-            }}
-          >
-            <button
-              style={{
-                alignSelf: 'flex-end'
-              }}
-              onClick={() => changeIndex(index - 1)}
-            >
-              Previous dog
-            </button>
-          </div>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              border: '1px solid black',
-              margin: '0 5px',
-              padding: 5
+              display: 'grid',
+              gridTemplateColumns: '1fr 4fr 1fr',
+              height: 300,
+              backgroundColor: 'pink'
             }}
           >
             <div
               style={{
-                display: 'grid',
-                placeItems: 'center'
+                display: 'flex',
+                justifyContent: 'flex-end'
               }}
             >
-              <img
+              <button
                 style={{
-                  height: 200,
-                  justifySelf: 'center'
+                  alignSelf: 'flex-start'
                 }}
-                src={dogs[index].url}
-              />
-              <p style={{ textAlign: 'center' }}>{dogs[index].title}</p>
+                onClick={() => changeIndex(index - 1)}
+              >
+                Previous dog
+              </button>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                margin: '0 5px',
+                padding: 5
+              }}
+            >
+              <div
+                style={{
+                  display: 'grid',
+                  placeItems: 'center'
+                }}
+              >
+                <img
+                  style={{
+                    height: 200,
+                    justifySelf: 'center'
+                  }}
+                  src={dogs[index].url}
+                />
+                <p style={{ textAlign: 'center' }}>{dogs[index].title}</p>
+                <div></div>
+              </div>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'flex-start'
+              }}
+            >
+              <button
+                style={{
+                  alignSelf: 'flex-start'
+                }}
+                onClick={() => changeIndex(index + 1)}
+              >
+                Next dog
+              </button>
             </div>
           </div>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'flex-start'
-            }}
-          >
-            <button
+
+          <div>
+            <div
               style={{
-                alignSelf: 'flex-end'
+                // display: "flex",
+                // justifyContent: "left",
+                // margin: "0 5px",
+                // padding: 5,
+                // paddingLeft: 200,
+                // paddingRight: 200,
+                backgroundColor: 'lightblue'
               }}
-              onClick={() => changeIndex(index + 1)}
             >
-              Next dog
-            </button>
+              <div
+                style={{
+                  display: 'flex',
+                  // display: "grid"
+                  // placeItems: "center"
+                  justifyContent: 'center'
+                }}
+              >
+                <div></div>
+                <input
+                  style={{
+                    width: '80%',
+                    paddingLeft: 5,
+                    paddingRight: 5
+                  }}
+                  value={comment}
+                  type='text'
+                  onChange={e => setComment(e.target.value)}
+                />
+                <br />
+                <button onClick={addComment}>Add comment</button>
+              </div>
+            </div>
+            <div>
+              <h3>Comments</h3>
+              <ul style={{ listStyle: 'none' }}>
+                {dogs[index].comments.map(
+                  ({ comment, upvoteCount }, commentIndex) => (
+                    <li>
+                      {comment}
+                      {/* commentIndex: {commentIndex} */}
+                      <p>
+                        Votes: {upvoteCount}{' '}
+                        <button onClick={() => increaseVote(commentIndex)}>
+                          Vote
+                        </button>
+                      </p>
+                    </li>
+                  )
+                )}
+              </ul>
+            </div>
           </div>
         </div>
       ) : (
